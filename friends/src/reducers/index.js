5eac4d25d9298cca672fb9f36d8d99c6505040b1
jjgorton <jjgorton@gmail.com>
2019-04-11 17:20:39 -0400

    store set-up, reducer started

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,14 @@
+//import action types
+
+const initialState = {
+	friends : []
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default reducer;
